24c04144b455fac93b199175dfd55a51378faec9
Sammy-Barasa <kesammybarasa@gmail.com>
2023-04-24 13:26:08 +0300

    added navigation

--- WebApp/loancalculatorweb/src/App.js
+++ WebApp/loancalculatorweb/src/App.js
@@ -1,14 +1,37 @@
 
 import './App.css';
+import { Routes,Route,Link,NavLink } from 'react-router-dom';
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import LoansPage from './pages/LoansPage';
+import ApiPage from './pages/ApiPage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p className="App-title">
-          Loan Calculator
+          <NavLink to="/">Loan Calculator</NavLink>
         </p>
+        <div>
+          <div><Link to="/signin">Login</Link></div>
+          <div><Link to="/signup">SignUp</Link></div>
+          <div><Link to="/api">API</Link></div>
+        </div>
       </header>
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/signin" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/loans" element={<LoansPage/>}/>
+          <Route path="/api" element={<ApiPage/>}/>
+        </Routes>
+      </div>
+      <footer>
+        <p>@loancalulator</p>
+      </footer>
     </div>
   );
 }
