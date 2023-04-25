79acbe98d152e4c3cda76387045e9ddce45a3256
Sammy-Barasa <kesammybarasa@gmail.com>
2023-04-26 02:09:15 +0300

    corrected traiing slash in auth app

--- BackEnd/authentication/urls.py
+++ BackEnd/authentication/urls.py
@@ -6,9 +6,9 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path("register", RegisterView.as_view(), name="register_endpoint"),
-    path("verify_email", VerifyEmailView.as_view(), name="verify_endpoint"),
-    path("login", LoginView.as_view(), name="login_endpoint"),
+    path("register/", RegisterView.as_view(), name="register_endpoint"),
+    path("verify_email/", VerifyEmailView.as_view(), name="verify_endpoint"),
+    path("login/", LoginView.as_view(), name="login_endpoint"),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
