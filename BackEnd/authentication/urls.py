988d01a8cabb70780b4ba45b0bc8817c671f1096
Sammy-Barasa <kesammybarasa@gmail.com>
2023-04-28 11:31:30 +0300

    adding social authentication backend routes

--- BackEnd/authentication/urls.py
+++ BackEnd/authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls.conf import path
-from authentication.views import RegisterView, VerifyEmailView, LoginView
+from authentication.views import RegisterView, VerifyEmailView, LoginView,SocialLoginView
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenObtainPairView,
@@ -9,6 +9,7 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register_endpoint"),
     path("verify_email/", VerifyEmailView.as_view(), name="verify_endpoint"),
     path("login/", LoginView.as_view(), name="login_endpoint"),
+    path("login/social/", SocialLoginView.as_view(), name="social_login_endpoint"),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
